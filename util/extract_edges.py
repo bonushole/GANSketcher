fc0c8d16dec75f301aa07a4c01966b7fd0f3bb33
Acstry <59457161+Acstry@users.noreply.github.com>
2022-04-26 16:53:00 -0400

    New extraction function created if we have time to use it for our testing

--- util/extract_edges.py
+++ util/extract_edges.py
@@ -1,5 +1,7 @@
 from PIL import Image, ImageFilter
 import PIL.ImageOps
+import cv2
+import numpy as np
 
 MY_FILTER = ImageFilter.Kernel((5, 5),
     (-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,  22, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,), 1, 0)
@@ -12,3 +14,13 @@ def extract_edges(image):
     image = PIL.ImageOps.invert(image)
     return image
 
+def new_extract(cv_img):
+    blur = cv2.GaussianBlur(cv_img, (3, 3), 0)
+    sigma = np.std(blur)
+    mean = np.mean(blur)
+    lower = int(max(0, (mean - sigma)))
+    upper = int(min(255, (mean + sigma)))
+
+    edge = cv2.Canny(blur, lower, upper)
+    edge = cv2.bitwise_not(edge)
+    return edge
